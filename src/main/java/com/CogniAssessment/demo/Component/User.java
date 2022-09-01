83cbf805149a9f2f1d2dbf9fd1124df590a59145
Shane Chia <u1120533@umail.utah.edu>
2022-09-01 13:25:09 +0800

    Added in a Component and Controller
    -Added a User Component that contains information on the user to sign in
    -Added a User Controller for the user client to access the server and sign in

---
+++ src/main/java/com/CogniAssessment/demo/Component/User.java
@@ -0,0 +1,19 @@
+package com.CogniAssessment.demo.Component;
+
+public class User {
+    private long id;
+    private String username;
+    
+    public User(long id, String username) {
+		this.id = id;
+		this.username = username;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+}
---
+++ src/main/java/com/CogniAssessment/demo/Controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.CogniAssessment.demo.Controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.CogniAssessment.demo.Component.User;
+
+@RestController
+public class UserController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/signin")
+	public User greeting(@RequestParam(value = "name", defaultValue = "Anon") String name) {
+		return new User(counter.incrementAndGet(), name);
+	}
+}
